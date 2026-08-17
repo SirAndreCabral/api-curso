06036e7f41455f29353e601b340fe8a92d9fb115
AndreCabral <andre_kbral@hotmail.com>
2026-08-17 14:08:49 -0300

    fix: configure integration tests for CI

--- src/test/java/org/example/apicursos/ApiCursosApplicationTests.java
+++ src/test/java/org/example/apicursos/ApiCursosApplicationTests.java
@@ -2,12 +2,13 @@ package org.example.apicursos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
 
+@ImportTestcontainers
 @SpringBootTest
 class ApiCursosApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
